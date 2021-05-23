a3daceb2fc32c9cd0d9277e32e8cf09c243c6e89
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-05-23 13:53:56 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(work="go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair = "*"){
+    return function inner(hype = "special"){
+        return `You are ${flair}${hype}${flair}!`
+    }
+}
